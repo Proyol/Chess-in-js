214e567aee588f13cd55356c1814bb26e016e264
Proyol <52104910+Proyol@users.noreply.github.com>
2020-05-12 11:44:23 -0300

    New functions
    Added a mouseOver function that draws a stroke rectangle on the board

--- cell.js
+++ cell.js
@@ -1,4 +1,10 @@
 const cw = 65;
+let x, y;
+
+document.addEventListener('mousemove', function(event){
+    x = event.clientX;
+    y = event.clientY;
+});
 
 class Cell{
     constructor(x, y, Piece, color){
@@ -6,12 +12,22 @@ class Cell{
         this.y = y;
         this.Piece = Piece;
         this.color = color;
+        this.mouseIsOver = false;
+    }
+
+    mouseOver(){
+        if(x > this.x &&
+           x < this.x + cw &&
+           y > this.y &&
+           y < this.y + cw){
+            ctx.strokeStyle = 'red';
+            ctx.lineWidth = 5;
+            ctx.strokeRect(this.x, this.y, cw, cw);
+        }
     }
 
     display(){
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.x + cw, this.y + cw);
+        ctx.fillRect(this.x, this.y, cw, cw);
     }
 }
-
-Cell.prototype.constructor = Cell;
